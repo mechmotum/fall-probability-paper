1e212c88c986fe2e0ba4824a86b817d3ab040873
Jason K. Moore <moorepants@gmail.com>
2024-08-11 13:23:32 +0200

    Shade stable region grey, improve axes labels.

--- src/control.py
+++ src/control.py
@@ -71,30 +71,32 @@ def plot_eig(ax, model, kphidots=0.0):
                                                        kphidot=kphidots))
     weave_idx, capsize_idx = stable_ranges(evals_)[0]
     weave_speed, capsize_speed = speeds[weave_idx], speeds[capsize_idx]
-    print('Weave speed: {:1.2f} [m/s]'.format(weave_speed))
-    print('Capsize speed: {:1.2f} [m/s]'.format(capsize_speed))
+    msg = 'Weave speed: {:1.2f} [m/s], {:1.1f} [km/h]'
+    print(msg.format(weave_speed, weave_speed*MPS2KPH))
+    msg = 'Capsize speed: {:1.2f} [m/s], {:1.1f} [km/h]'
+    print(msg.format(capsize_speed, capsize_speed*MPS2KPH))
     ax.axvline(6.0*KPH2MPS, ymin=-10.0, ymax=10.0)
     ax.axvline(10.0*KPH2MPS, ymin=-10.0, ymax=10.0)
     model.plot_eigenvalue_parts(ax=ax, v=speeds, kphidot=kphidots,
                                 colors=['k']*4)
     ax.fill_between(speeds, -10, 10, where=np.all(evals_.real < 0.0, axis=1),
-                    color='green', alpha=0.4,
+                    color='gray', alpha=0.4,
                     transform=ax.get_xaxis_transform())
     ax.set_ylim((-10, 10))
     return ax
 
 
 ax = plot_eig(axes[0, 0], model_without)
-ax.set_title('Without Rider')
-ax.set_ylabel('Uncontrolled\nEigenvalue Components [1/s]')
+ax.set_title('Without Rigid Rider')
+ax.set_ylabel('Balance Assist Off\nEigenvalue Components [1/s]')
 ax.set_xlabel('')
 
 ax = plot_eig(axes[1, 0], model_without, kphidots=kphidots)
-ax.set_ylabel('Controlled\nEigenvalue Components [1/s]')
+ax.set_ylabel('Balance Assist On\nEigenvalue Components [1/s]')
 ax.set_xlabel('Speed [m/s]')
 
 ax = plot_eig(axes[0, 1], model_with)
-ax.set_title('With Rider')
+ax.set_title('With Rigid Rider')
 ax.set_ylabel('')
 ax.set_xlabel('')
 
